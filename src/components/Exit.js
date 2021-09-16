980ffd55995daf82611a4666c523c38fad5e569d
Julio Franco <julio.franco9111@gmail.com>
2021-09-16 00:12:47 -0300

    fix ui and add mediaqueries for modal

--- src/components/Exit.js
+++ src/components/Exit.js
@@ -1,20 +1,16 @@
-import React from 'react'
-import { Button } from './Button'
+import React from 'react';
+import { Button } from './Button';
 
 export const Exit = () => {
-    return (
-        <div className="layout__page">
-            <h1 className='title'>¿Quieres salir?</h1>
+  return (
+    <div className='layout__page'>
+      <h1 className='title'>¿Quieres salir?</h1>
 
-
-
-            <div className="puzzle__button">
-            <p>Al salir perderás todo tu progreso</p>
-            <br/>
-                <Button msg="Si, salir" color='white'/>
-                <br/>
-                <Button  msg="No, seguir jugando" color='black' />
-            </div>
-        </div>
-    )
-}
+      <div className='exit__buttons'>
+        <p>Al salir perderás todo tu progreso</p>
+        <Button msg='Si, salir' color='white' />
+        <Button msg='No, seguir jugando' color='black' />
+      </div>
+    </div>
+  );
+};
